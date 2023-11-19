dae87f9a6505560f35e8ff9e7c67514a81f85335
Ir4D <iradedova@gmail.com>
2023-11-19 21:54:12 +0200

    feat: update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,35 @@
-import { Provider } from 'react-redux';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, MemoryRouter } from 'react-router-dom';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
 import MainPage from './components/pages/main-page';
 import Details from './components/item-details/item-details';
 import toolkitSlice from './reducer';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import ResultsList from './components/results-list/results-list';
+import { apiService } from './services/api-request';
+import ResultItem from './components/results-list/result-item';
+import toolkitReducer, {
+  changeSearchTerm,
+  changeResultList,
+  changeItemsPerPage,
+  changeLoadingMainPage,
+  changeLoadingDetailsPage,
+} from './reducer';
 
 describe('App', () => {
-  it('renders App component', async () => {
+  test('renders App component', async () => {
     const rootReducer = combineReducers({
       toolkit: toolkitSlice,
+      [apiService.reducerPath]: apiService.reducer,
     });
 
     const store = configureStore({
       reducer: rootReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiService.middleware),
     });
 
     render(
@@ -31,230 +46,234 @@ describe('App', () => {
   });
 });
 
-// test('renders ResultsList (CardList)', () => {
-//   render(
-//     <ResultsList
-//       page={0}
-//       setPage={function (): void {
-//         throw new Error('Function not implemented.');
-//       }}
-//       itemsPerPage={0}
-//       setItemsPerPage={function (): void {
-//         throw new Error('Function not implemented.');
-//       }}
-//     />
-//   );
-// });
+describe('MainPage', () => {
+  const rootReducer = combineReducers({
+    toolkit: toolkitSlice,
+    [apiService.reducerPath]: apiService.reducer,
+  });
 
-// describe('MainPage', () => {
-//   test('renders MainPage', () => {
-//     render(
-//       <Router>
-//         <MainPage
-//           term={''}
-//           setTerm={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//           updateData={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//           resultsList={[]}
-//           setResultList={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//         />
-//       </Router>
-//     );
-//     expect(screen.getByText('Anime List:')).toBeInTheDocument();
-//   });
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiService.middleware),
+  });
 
-//   test('updates term when search input changes', () => {
-//     render(
-//       <Router>
-//         <MainPage
-//           term={''}
-//           setTerm={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//           updateData={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//           resultsList={[]}
-//           setResultList={function (): void {
-//             throw new Error('Function not implemented.');
-//           }}
-//         />
-//       </Router>
-//     );
-//     const searchInput = screen.getByPlaceholderText('Type here');
-//     fireEvent.change(searchInput, { target: { value: 'Naruto' } });
-//     expect(searchInput).toHaveValue('Naruto');
-//   });
-// });
+  test('renders MainPage', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/']}>
+          <Routes>
+            <Route path="/rsschool_REACT2023Q4/" element={<MainPage />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByText('Anime List:')).toBeInTheDocument();
+  });
 
-// jest.mock('./services/api-service', () => ({
-//   __esModule: true,
-//   ...jest.requireActual('./services/api-service'),
-//   getItemDetails: jest.fn(),
-//   getAllItems: jest.fn(),
-// }));
+  test('updates term when search input changes', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/']}>
+          <Routes>
+            <Route path="/rsschool_REACT2023Q4/" element={<MainPage />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>
+    );
+    const searchInput = screen.getByPlaceholderText('Type here');
+    fireEvent.change(searchInput, { target: { value: 'Naruto' } });
+    expect(searchInput).toHaveValue('Naruto');
+  });
+});
 
-// describe('Tests for the Card List component:', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks();
-//   });
+const server = setupServer(
+  rest.get('https://api.jikan.moe/v4/anime', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: [
+          {
+            mal_id: 1,
+            title: 'Sample Anime 1',
+            year: 2022,
+            type: 'TV',
+            images: { jpg: { image_url: 'https://example.com/image1.jpg' } },
+            image_url: 'https://example.com/image1.jpg',
+          },
+          {
+            mal_id: 2,
+            title: 'Sample Anime 2',
+            year: 2022,
+            type: 'Movie',
+            images: { jpg: { image_url: 'https://example.com/image2.jpg' } },
+            image_url: 'https://example.com/image2.jpg',
+          },
+        ],
+      })
+    );
+  })
+);
 
-//   test('Check that an appropriate message is displayed if no cards are present', async () => {
-//     const mockResults = [
-//       {
-//         id: 1,
-//         title: 'Sample Anime',
-//         year: 2022,
-//         img: 'sample-image-url',
-//       },
-//     ];
+beforeAll(() => server.listen());
+afterEach(() => {
+  server.resetHandlers();
+  jest.clearAllMocks();
+});
+afterAll(() => server.close());
 
-//     (
-//       jest.requireMock('./services/api-service') as {
-//         getAllItems: jest.Mock<Promise<unknown>, [number, number]>;
-//       }
-//     ).getAllItems.mockResolvedValue(mockResults);
+describe('Tests for the Card List component:', () => {
+  const rootReducer = combineReducers({
+    toolkit: toolkitSlice,
+    [apiService.reducerPath]: apiService.reducer,
+  });
 
-//     const contextValues: ContextProps = {
-//       term: '',
-//       setTerm: jest.fn(),
-//       updateData: jest.fn(),
-//       resultsList: [],
-//       setResultList: jest.fn(),
-//     };
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiService.middleware),
+  });
 
-//     render(
-//       <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/']}>
-//         <Context.Provider value={contextValues}>
-//           <Routes>
-//             <Route
-//               path="/rsschool_REACT2023Q4/"
-//               element={
-//                 <MainPage
-//                   term={''}
-//                   setTerm={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                   updateData={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                   resultsList={[]}
-//                   setResultList={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                 />
-//               }
-//             />
-//           </Routes>
-//         </Context.Provider>
-//       </MemoryRouter>
-//     );
+  test('Check that an appropriate message is displayed if no cards are present', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/']}>
+          <Routes>
+            <Route path="/rsschool_REACT2023Q4/" element={<MainPage />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>
+    );
 
-//     expect(screen.getByTestId('spinner')).toBeInTheDocument();
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
 
-//     await waitFor(() => {
-//       expect(screen.queryByTestId('spinner')).not.toBeInTheDocument();
-//     });
-//     expect(screen.getByText('No anime were found')).toBeInTheDocument();
-//   });
-// });
+    await waitFor(() => {
+      expect(screen.queryByTestId('spinner')).not.toBeInTheDocument();
+    });
+    expect(screen.getByText('No anime were found')).toBeInTheDocument();
+  });
 
-// describe('tests for Details Component', () => {
-//   test('Check that a loading indicator is displayed while fetching data', async () => {
-//     const mockResults = [
-//       {
-//         id: 1,
-//         title: 'Sample Anime',
-//         year: 2022,
-//         img: 'sample-image-url',
-//       },
-//     ];
+  test('renders spinner when fetching data', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ResultsList page={1} setPage={() => {}} />
+        </MemoryRouter>
+      </Provider>
+    );
 
-//     (
-//       jest.requireMock('./services/api-service') as {
-//         getItemDetails: jest.Mock<Promise<unknown>, [number, number]>;
-//       }
-//     ).getItemDetails.mockResolvedValueOnce(mockResults);
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
 
-//     const contextValues: ContextProps = {
-//       term: '',
-//       setTerm: jest.fn(),
-//       updateData: jest.fn(),
-//       resultsList: [],
-//       setResultList: jest.fn(),
-//     };
+    await waitFor(() => {
+      expect(screen.queryByTestId('spinner')).not.toBeInTheDocument();
+    });
+  });
+});
 
-//     render(
-//       <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/']}>
-//         <Context.Provider value={contextValues}>
-//           <Routes>
-//             <Route
-//               path="/rsschool_REACT2023Q4/"
-//               element={
-//                 <MainPage
-//                   term={''}
-//                   setTerm={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                   updateData={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                   resultsList={[]}
-//                   setResultList={function (): void {
-//                     throw new Error('Function not implemented.');
-//                   }}
-//                 />
-//               }
-//             />
-//           </Routes>
-//         </Context.Provider>
-//       </MemoryRouter>
-//     );
+const mockAnime = {
+  mal_id: 1,
+  title: 'Sample Anime',
+  year: 2022,
+  type: 'TV',
+  images: { jpg: { image_url: 'https://example.com/image.jpg' } },
+  image_url: 'https://example.com/image.jpg',
+};
 
-//     await waitFor(() => {
-//       expect(screen.queryByTestId('spinner')).toBeNull();
-//     });
-//   });
-// });
+test('renders anime item correctly', () => {
+  const rootReducer = combineReducers({
+    toolkit: toolkitSlice,
+    [apiService.reducerPath]: apiService.reducer,
+  });
 
-// describe('Details Component', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiService.middleware),
+  });
 
-//   test('renders details', async () => {
-//     const mockGetItemDetails = jest.spyOn(apiService, 'getItemDetails');
-//     mockGetItemDetails.mockResolvedValue({
-//       id: 1,
-//       title: 'Sample Anime',
-//       titleJp: 'Sample Japanese Title',
-//       year: 2022,
-//       type: 'TV',
-//       score: 8.5,
-//       rating: 'PG-13',
-//       img: 'sample-image-url',
-//     });
+  render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <ResultItem anime={mockAnime} />
+      </MemoryRouter>
+    </Provider>
+  );
 
-//     render(
-//       <MemoryRouter initialEntries={['/rsschool_REACT2023Q4/details/1']}>
-//         <Routes>
-//           <Route
-//             path="/rsschool_REACT2023Q4/details/:id"
-//             element={<Details />}
-//           />
-//         </Routes>
-//       </MemoryRouter>
-//     );
+  const animeTitle = screen.getByText('Sample Anime');
+  const animeYear = screen.getByText('Year: 2022');
+  const animeImage = screen.getByAltText('Anime');
 
-//     await waitFor(() => {
-//       expect(screen.queryByTestId('spinner')).toBeNull();
-//     });
+  expect(animeTitle).toBeInTheDocument();
+  expect(animeYear).toBeInTheDocument();
+  expect(animeImage).toBeInTheDocument();
+  expect(animeImage).toHaveAttribute('src', 'https://example.com/image.jpg');
+});
 
-//     expect(screen.getByText('Close')).toBeInTheDocument();
-//   });
-// });
+test('clicking on anime item sets selected anime', () => {
+  const rootReducer = combineReducers({
+    toolkit: toolkitSlice,
+    [apiService.reducerPath]: apiService.reducer,
+  });
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiService.middleware),
+  });
+
+  const { container } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <ResultItem anime={mockAnime} />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  const animeItem = container.querySelector('.anime-item');
+  fireEvent.click(animeItem!);
+});
+
+describe('Tests for reducer file', () => {
+  const initialState = {
+    searchTerm: '',
+    resultsList: [],
+    itemsPerPage: 12,
+    loadingMainPage: false,
+    loadingDetailsPage: false,
+  };
+
+  test('handle changeSearchTerm action', () => {
+    const nextState = toolkitReducer(
+      initialState,
+      changeSearchTerm('New Search Term')
+    );
+    expect(nextState.searchTerm).toBe('New Search Term');
+  });
+
+  test('handle changeResultList action', () => {
+    const mockResultList = [{ id: 1, title: 'Sample Anime' }];
+    const nextState = toolkitReducer(
+      initialState,
+      changeResultList(mockResultList)
+    );
+    expect(nextState.resultsList).toEqual(mockResultList);
+  });
+
+  test('handle changeItemsPerPage action', () => {
+    const nextState = toolkitReducer(initialState, changeItemsPerPage(24));
+    expect(nextState.itemsPerPage).toBe(24);
+  });
+
+  test('handle changeLoadingMainPage action', () => {
+    const nextState = toolkitReducer(initialState, changeLoadingMainPage(true));
+    expect(nextState.loadingMainPage).toBe(true);
+  });
+
+  test('handle changeLoadingDetailsPage action', () => {
+    const nextState = toolkitReducer(
+      initialState,
+      changeLoadingDetailsPage(true)
+    );
+    expect(nextState.loadingDetailsPage).toBe(true);
+  });
+});
