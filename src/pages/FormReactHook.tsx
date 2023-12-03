18ac92c1a976180ca6005f16ee1e8ea1fe2739f3
Ir4D <iradedova@gmail.com>
2023-12-03 22:05:02 +0200

    feat: add partial validation

--- src/pages/FormReactHook.tsx
+++ src/pages/FormReactHook.tsx
@@ -17,28 +17,73 @@ type FormTypes = {
 
 const FormReactHook = () => {
   const form = useForm<FormTypes>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
 
   const onSubmit = (data: FormTypes) => {
     console.log(data);
+    errors;
   };
 
   return (
     <>
       <h1 className="form-title rHookForm-title">React Hook Form</h1>
       <div className="form rHookForm-form">
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
           <div className="form-field form-name rHookForm-name">
             <label htmlFor="nameR">Name:</label>
-            <input type="text" id="nameR" {...register('nameR')} />
+            <input
+              type="text"
+              id="nameR"
+              {...register('nameR', {
+                required: {
+                  value: true,
+                  message: 'Name is required',
+                },
+                pattern: {
+                  value: /^[A-Z]/,
+                  message: 'First letter must be uppercased',
+                },
+              })}
+            />
+            <p>{errors.nameR?.message}</p>
           </div>
           <div className="form-field form-age rHookForm-age">
             <label>Age:</label>
-            <input type="number" id="ageR" {...register('ageR')} />
+            <input
+              type="number"
+              id="ageR"
+              {...register('ageR', {
+                required: {
+                  value: true,
+                  message: 'Age is required',
+                },
+                pattern: {
+                  value: /^[0-9]/,
+                  message: 'Age should be a non-negative number',
+                },
+              })}
+            />
+            <p>{errors.ageR?.message}</p>
           </div>
           <div className="form-field form-email rHookForm-email">
             <label>Email:</label>
-            <input type="email" id="emailR" {...register('emailR')} />
+            <input
+              type="email"
+              id="emailR"
+              {...register('emailR', {
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: 'Invalid email format',
+                },
+                required: {
+                  value: true,
+                  message: 'Email is required',
+                },
+              })}
+            />
+            <p>{errors.emailR?.message}</p>
           </div>
           <div className="form-field form-psw rHookForm-psw">
             <label>Password:</label>
